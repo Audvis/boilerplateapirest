ab3f1bb87d4790e5907879e172e8e6b583cf618d
Gerardo Pedraza <72670178+Audvis@users.noreply.github.com>
2021-10-16 13:49:03 -0500

    Update Prueba.js

--- routes/Prueba.js
+++ routes/Prueba.js
@@ -4,31 +4,10 @@ const {
     getController,              
 } = require('../controllers/Prueba/getController');
 
-const {
-    postController,
-} = require('../controllers/Prueba/postController');
-
-// const {
-//     putController
-// } = require('../controllers/Prueba/putController');
-
-// const {
-//     deleteController
-// } = require('../controllers/Prueba/deleteController');
-
-
 
 //GET
 router.get('/get',getController);
 
-//POST
-router.post('/post',postController);
-
-//PUT
-// router.put('/put/:id',putController)
-
-//DELETE
-// router.delete('/delete/:id',deleteController)
 
 
 module.exports = router;
