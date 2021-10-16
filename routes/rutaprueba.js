f0040a90b85b1fbb3991c54b24bf8f3db86af168
Audvis <artistaudiovisual@gmail.com>
2021-10-16 15:23:03 -0500

    new

--- routes/rutaprueba.js
+++ routes/rutaprueba.js
@@ -4,7 +4,7 @@ const {testcontroller} = require('../controllers/Prueba/testcontroller');
 const {postPrueba} = require('../controllers/Prueba/postController');
 const {getPrueba} = require('../controllers/Prueba/getController');
 const {putPrueba} = require('../controllers/Prueba/putController');
-// const {deletePrueba} = require('../controllers/Prueba/deleteController');
+
 
 
 router.get('/',
@@ -22,7 +22,7 @@ router.get('/get',
 )
 
 // put
-router.put('/get/:id',
+router.put('/put/:id',
    putPrueba 
 )
 
