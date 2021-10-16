3134bf74231a79613232b268c7e97d4dcade7feb
Audvis <artistaudiovisual@gmail.com>
2021-10-16 14:45:30 -0500

    ultimas pruebas

--- controllers/Prueba/getController.js
+++ controllers/Prueba/getController.js
@@ -3,7 +3,7 @@ const Prueba = require('../../database/models/Prueba');
 exports.getPrueba = async (req,res) => {
     try {
         let pruebas = await Prueba.findAll({
-            attributes: ['name', 'description']
+            attributes: ['id', 'name', 'description']
         })
         res.json(pruebas)
     } catch (error) {
